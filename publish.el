d1798a237239a4823fc6ee62d45f595595cf4454
Raj <rajp152000@gmail.com>
2023-07-12 12:11:04 +0530

    removing search

--- publish.el
+++ publish.el
@@ -1,3 +1,4 @@
+;; for github action by : maker2413/OrgRoamPublish@v1.0.1
 (package-initialize)
 
 ;; Install use-package for easy package installs
@@ -138,11 +139,11 @@
              "<form action='/'>\n"
              "<input type='submit' value='Home'>\n"
              "</form>\n"
-             "<form id='search' autocomplete='off' class='search-box' action=/search.html>\n"
-             "<label hidden for='search-input'></label>\n"
-             "<input type='text' id='search-input' name='query' placeholder='Type here to search'>\n"
-             "<input type='submit' value='search'>\n"
-             "</form>\n"
+             ;;"<form id='search' autocomplete='off' class='search-box' action=/search.html>\n"
+             ;;"<label hidden for='search-input'></label>\n"
+             ;;"<input type='text' id='search-input' name='query' placeholder='Type here to search'>\n"
+             ;;"<input type='submit' value='search'>\n"
+             ;;"</form>\n"
              "</div>\n"
              "</header>\n"
              "<div class='notes-container'>\n"

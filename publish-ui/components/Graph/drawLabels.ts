7ba21c7666667818daf331f1bf89e5ef5960a322
Raj Patil <rajp152k@gmail.com>
2023-08-11 14:22:59 +0530

    changed import from force-graph to react-force-graph in components/Graph/drawlabels.ts

--- publish-ui/components/Graph/drawLabels.ts
+++ publish-ui/components/Graph/drawLabels.ts
@@ -1,5 +1,5 @@
 import { OrgRoamNode } from '../../api'
-import { NodeObject } from 'force-graph'
+import { NodeObject } from 'react-force-graph'
 import { initialVisuals } from '../config'
 import { LinksByNodeId } from '../../pages'
 import wrap from 'word-wrap'
